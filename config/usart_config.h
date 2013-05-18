b02e91e32d39b9283edc035f2fc8ae5e112a3bdd
icke <icke@X230iSSD>
2013-05-18 11:20:43 +0200

    add usart

---
+++ config/usart_config.h
@@ -0,0 +1,16 @@
+/*
+ * config_usart.h
+ *
+ *  Created on: 12.05.2013
+ *      Author: icke
+ */
+
+#ifndef USART_CONFIG_H_
+#define USART_CONFIG_H_
+
+#ifndef F_CPU
+	#define F_CPU 1000000UL
+#endif
+
+#define BAUDRATE 19200 // -> Handy Siemens C35is
+#endif /* CONFIG_USART_H_ */
