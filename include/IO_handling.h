8da688aae0b071c00d4cdec7496ba71adff34552
icke2063 <icke@X230iSSD>
2014-03-11 00:03:11 +0100

    disable uart

--- include/IO_handling.h
+++ include/IO_handling.h
@@ -13,10 +13,10 @@
 
 #ifndef IO_HANDLING_H_
 #define IO_HANDLING_H_
-#include "usart.h"
+//#include "usart.h"
 
-#define IO_DEBUG usart_write
-//#define IO_DEBUG(...)
+//#define IO_DEBUG usart_write
+#define IO_DEBUG(...)
 
 //#define READ_IO_DEBUG IO_DEBUG
 #define READ_IO_DEBUG(...)
